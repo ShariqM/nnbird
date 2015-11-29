6e7dbea63ee98af279ec3475fb735af6f039b7ec
Shariq Mobin <Shariq.Mobin@gmail.com>
2015-11-28 21:58:13 -0800

    make bias 0 to get SHC

--- shc.py
+++ shc.py
@@ -3,18 +3,22 @@ import matplotlib.pyplot as plt
 import pdb
 import math
 
-simulate_for = 200
+simulate_for = 5
 x = np.zeros(simulate_for)
 v = np.zeros(simulate_for)
-x[0] = 1
-v[0] = 0.2
+x[0] = 1.0
+v[0] = 0.0
 dt = 0.01
 k = math.sqrt(math.pi/2)
 k = 50
 
 for i in range(1, simulate_for):
-    v[i] = v[i-1] - k * x[i-1] * dt
+    vdot = -k * x[i-1]
+    v[i] = v[i-1] + vdot * dt
     x[i] = x[i-1] + v[i] * dt
+    print "x=%.3f, v=%.3f, vdot=%.3f" % (x[i], v[i], vdot)
+    #if i > 10:
+        #break
 
 plt.plot(x)
 plt.show()
--- train.lua
+++ train.lua
@@ -5,17 +5,50 @@ require 'optim'
 require 'lfs'
 require 'gnuplot'
 
+local model_utils=require 'model_utils'
+local ENN = require 'models.enn'
 
 dt = 1 / 16000
-length = 30 * 0.001 -- 30 ms)
+length = 30 * 0.001 -- 30 ms
 seq_length = length / dt
 
+x_0 = 1.0
+v_0 = 0.0
+k = -50
+dt = 0.01
+seq_length = 200 -- FIXME
+
 protos = {}
-protos.ernn = ERNN.ernn()
+protos.enn = ENN.enn()
 protos.criterion = nn.MSECriterion()
 
+params, grad_params = model_utils.combine_all_parameters(protos.enn)
+params[1] = k
+params[2] = 0 -- 0 bias
+print('number of parameters in the model: ' .. params:nElement())
 
+clones = {}
 for name,proto in pairs(protos) do
     print('cloning ' .. name)
     clones[name] = model_utils.clone_T_times(proto, seq_length)
 end
+
+dt_tensor = torch.DoubleTensor(1,1):fill(dt)
+x  = torch.DoubleTensor(1,1):fill(x_0)
+v  = torch.DoubleTensor(1,1):fill(v_0)
+xv = torch.DoubleTensor(seq_length)
+
+function sleep(n)
+    os.execute("sleep " .. tonumber(n))
+end
+
+for t=1, seq_length do
+    x, v, vdot = unpack(clones.enn[t]:forward{x, v, dt_tensor})
+    print (string.format("x=%.3f,v=%.3f, vdot=%.3f", x[{1,1}], v[{1,1}], vdot[{1,1}]))
+    xv[t] = x[{1,1}]
+end
+
+gnuplot.plot(xv)
+sleep(100)
+
+print('number of parameters in the model: ' .. params:nElement())
