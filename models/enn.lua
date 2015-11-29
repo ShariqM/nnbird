bce6d715f2c33b7854bfaf0f372bbc8afc86c936
Shariq Mobin <Shariq.Mobin@gmail.com>
2015-11-28 21:58:23 -0800

    make bias 0 to get SHC

---
+++ models/enn.lua
@@ -0,0 +1,15 @@
+local ENN = {}
+
+function ENN.enn()
+    local x  = nn.Identity()() -- Past
+    local v  = nn.Identity()() -- Past
+    local dt = nn.Identity()()
+
+    local vdot = nn.Linear(1,1)(x) -- (-kx)
+    local v_t  = nn.CAddTable()({v, nn.CMulTable()({vdot,dt})})
+    local x_t  = nn.CAddTable()({x, nn.CMulTable()({v_t,dt})})
+
+    return nn.gModule({x, v, dt}, {x_t, v_t, vdot})
+end
+
+return ENN
